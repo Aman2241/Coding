0fd6e962c7aa2d5406b9ac4d108cf45eff7bdbb7
Aman Raj <amanrajsingh7870865175@gmail.com>
2020-05-10 00:19:13 +0530

    Create perfect_sq.cpp

---
+++ Day 1/perfect_sq.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int n;
+cin>>n;
+double x=sqrt(n);
+
+if(x-floor(x)==0)
+	cout<<"Yes"<<endl;
+else
+	cout<<"No"<<endl;
+	
+
+return 0;
+
+}
