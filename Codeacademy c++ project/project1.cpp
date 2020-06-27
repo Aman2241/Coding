51591a37a8b9e96ccbf907cd999e16f2e9665600
Aman Raj <amanrajsingh7870865175@gmail.com>
2020-06-27 23:16:56 +0530

    Practice

---
+++ Codeacademy c++ project/project1.cpp
@@ -0,0 +1,203 @@
+//My initials are A and R.
+
+
+//1st project -> writing initials of my name A and R in a given format
+
+
+
+
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int i=1;i<=1;i++)
+    {
+      for(int j=1;j<=5;j++)
+      {
+        if(j==3)
+          cout<<'A';
+        else
+          cout<<" ";
+      }
+      cout<<endl;
+    }
+    
+    
+    for(int i=1;i<=1;i++)
+    {
+      for(int j=1;j<=5;j++)
+      {
+        if(j==2 || j==4)
+          cout<<'A';
+        else
+          cout<<" ";
+      }
+      cout<<endl;
+    }
+    
+       for(int i=1;i<=1;i++)
+    {
+      for(int j=1;j<=5;j++)
+      {
+        if(j==1 || j==5)
+          cout<<'A';
+        else
+          cout<<" ";
+      }
+      cout<<endl;
+    }
+    
+       for(int i=1;i<=1;i++)
+    {
+      for(int j=1;j<=5;j++)
+      {
+        cout<<'A';
+      }
+      cout<<endl;
+    }
+    
+    
+    
+    
+      for(int i=1;i<=1;i++)
+    {
+      for(int j=1;j<=5;j++)
+      {
+        if(j==1 || j==5)
+          cout<<'A';
+        else
+          cout<<" ";
+      }
+      cout<<endl;
+    }
+    
+    
+    
+      for(int i=1;i<=1;i++)
+    {
+      for(int j=1;j<=5;j++)
+      {
+        if(j==1 || j==5)
+          cout<<'A';
+        else
+          cout<<" ";
+      }
+      cout<<endl;
+    }
+    
+    
+    
+      for(int i=1;i<=1;i++)
+    {
+      for(int j=1;j<=5;j++)
+      {
+        if(j==1 || j==5)
+          cout<<'A';
+        else
+          cout<<" ";
+      }
+      cout<<endl;
+    }
+    
+    
+    cout<<endl;
+    cout<<endl;
+    
+    
+    
+      for(int i=1;i<=1;i++)
+    {
+      for(int j=1;j<=5;j++)
+      {
+        if(j==1 || j==2 || j==3 || j==4)
+          cout<<'R';
+        else
+          cout<<" ";
+      }
+      cout<<endl;
+    }
+    
+    
+        for(int i=1;i<=1;i++)
+    {
+      for(int j=1;j<=5;j++)
+      {
+        if(j==1 || j==5)
+          cout<<'R';
+        else
+          cout<<" ";
+      }
+      cout<<endl;
+    }
+    
+    
+    
+        for(int i=1;i<=1;i++)
+    {
+      for(int j=1;j<=5;j++)
+      {
+        if(j==1 || j==5)
+          cout<<'R';
+        else
+          cout<<" ";
+      }
+      cout<<endl;
+    }
+    
+    
+         for(int i=1;i<=1;i++)
+    {
+      for(int j=1;j<=5;j++)
+      {
+        if(j==1 || j==2 || j==3 || j==4)
+          cout<<'R';
+        else
+          cout<<" ";
+      }
+      cout<<endl;
+    }
+    
+    
+    
+         for(int i=1;i<=1;i++)
+    {
+      for(int j=1;j<=5;j++)
+      {
+        if(j==1 || j==3)
+          cout<<'R';
+        else
+          cout<<" ";
+      }
+      cout<<endl;
+    }
+    
+    
+         for(int i=1;i<=1;i++)
+    {
+      for(int j=1;j<=5;j++)
+      {
+        if(j==1 ||j==4)
+          cout<<'R';
+        else
+          cout<<" ";
+      }
+      cout<<endl;
+    }
+    
+         for(int i=1;i<=1;i++)
+    {
+      for(int j=1;j<=5;j++)
+      {
+        if(j==1 ||j==5)
+          cout<<'R';
+        else
+          cout<<" ";
+      }
+      cout<<endl;
+    }
+    
+    
+    
+}
